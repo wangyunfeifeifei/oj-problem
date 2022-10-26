3606dd1352edc1bd4920a30c6fde75be14d22d9e
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2022-10-26 09:32:19 +0800

    Sunday算法

---
+++ algorithms/string/sumday.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+/**
+ * @brief 字符串匹配
+ *
+ * @param haystack 主串
+ * @param needle 模式串
+ * @return int 匹配位置
+ */
+int strStr(string haystack, string needle) {
+  int m = needle.size();
+  int n = haystack.size();
+  unordered_map<char, int> mov;
+  for (int i = 0; i < m; i++) {
+    mov[needle[i]] = m - i;
+  }
+  int i = m;
+  while (i <= n) {
+    int cnt = 0;
+    for (int j = m - 1; j >= 0 && needle[j] == haystack[i + j - m]; j--) {
+      cnt++;
+    }
+    if (cnt == m) {
+      return i - m;
+    }
+    if (mov.count(haystack[i]) != 0) {
+      i += mov[haystack[i]];
+    } else {
+      i += m;
+    }
+  }
+
+  return -1;
+}
+
+int main() {
+  string a = "a";
+  string b = "a";
+  cout << strStr(a, b) << endl;
+
+  return 0;
+}
